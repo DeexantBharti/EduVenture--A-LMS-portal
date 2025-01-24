4f3f62cd10b6db1fc01310b717536db409af7cee
DeexantBharti <deexantbharti4321@gmail.com>
2025-01-24 20:35:12 +0530

    educator route with outlet functionality.

--- src/pages/educator/Educator.jsx
+++ src/pages/educator/Educator.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import { Outlet } from 'react-router-dom'
 export default function Educator() {
   return (
     <div>
         <h1>
             Educator
-            
+
         </h1>
+        <div>
+      {<Outlet/>}
+        </div>
     </div>
   )
 }
